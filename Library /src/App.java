173f500319500c878f726114f166bfb3440abf79
aleksmiina <aleks.miina@gmail.com>
2025-04-17 20:22:03 +0000

    Updates in the code

--- Library /src/App.java
+++ Library /src/App.java
@@ -1,17 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        CollectionOfBooks books = new CollectionOfBooks();
-
-
+        
         System.out.println("Welcome to the Great Library of Kalimdor!");
         System.out.println("Pleae make your choice:");
-        System.out.println(books.); // need to print out the list of books from the enum 
+        
     }
 
-public class CollectionOfBooks {
-    public enum HistoryBooks {War and Peace, The Great War, The History of the World}
+    public class Book {
+        private String name;
+        private String author;
+        private int yearPublished;
+        private boolean isAvailable;
+
+        public Book (String name, String author, int yearPublished, boolean isAvailable){
+            
+        }
+
+    }
 }
 
 
-}
+
+
+
+
--- Library /src/notes.md
+++ Library /src/notes.md
@@ -4,9 +4,9 @@ ENUMs are constants and not strings!
 
 Library:
 
-list of books done via enum? but we can update the list so is this the best way to store books?
+- list of books will be done via a List
 
-user needs to ask for a book;
-program checks whether the book is available or exists in the library;
-then, program checks whether the book is available for borrowing;
-then, a user gets the book and it is unavailable anymore;
+- user needs to ask for a book;
+- program checks whether the book is available or exists in the library;
+- then, program checks whether the book is available for borrowing;
+- then, a user gets the book and it is unavailable anymore;
